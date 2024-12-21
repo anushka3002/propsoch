ea4e10732ae0143d6f164237018488e4ac4aba61
Anushka <anushka2457@gmail.com>
2024-12-21 17:00:17 +0530

    updated layouts

--- src/components/properites.jsx
+++ src/components/properites.jsx
@@ -14,7 +14,6 @@ import "slick-carousel/slick/slick-theme.css";
 import Footer from './Footer'
 import { useNavigate } from 'react-router'
 import { CustomNextArrow, CustomPrevArrow } from './commonComponent'
-import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
 const Properties = () => {
@@ -54,45 +53,105 @@ const Properties = () => {
         <>
             <div style={{ width: '100%' }}>
                 <div style={{ width: '100%' }}>
-                    <div style={{ height: '20px', width: '100%', textAlign: 'center', zIndex: 10, top: 0 }} className='header-fixed py-6'>
-                        <img src={propsoch} />
+                    <div
+                        style={{
+                            height: '20px',
+                            width: '100%',
+                            textAlign: 'center',
+                            zIndex: 10,
+                            top: 0,
+                        }}
+                        className='header-fixed py-6'
+                    >
+                        <img src={propsoch} alt="Propsoch Logo" />
                     </div>
-                    <div style={{ width: '100%' }} className='justify-between grid-container pb-12 mt-8'>{data?.data?.length > 0 ? data?.data?.map((e, index) => {
-                        return <div style={{ width: '256px', paddingLeft: '20px', paddingRight: '20px', outline: 'none', userSelect: 'none' }} className='relative cursor-pointer pb-6'>
-                            <Slider {...settings} className='scroll-container slider-container rounded-8'>
-                                {e?.images?.map((image) => {
-                                    return <img onClick={() => navigate(`/propertyDetails/${e._id}`)} style={{ width: '100%', height: 'auto', display: 'block', outline: 'none', userSelect: 'none' }} alt="img" src={image} className='' />
-                                })}
-                            </Slider>
-                            {e.typeOfRental != 'superhost' && <div style={{ top: 10, left: 30 }} className='mostLiked'>Most liked</div>}
-                            <div style={{ position: 'absolute', top: 10, right: 30 }}>
-                                <img style={{ outline: 'none', userSelect: 'none' }} alt="img" onClick={(el) => { handleWishlist(e); el.stopPropagation(); liked.includes(index) ? setLiked(liked.filter((el) => el !== index)) : setLiked([...liked, index]) }} width={'30px'} src={liked.includes(index) ? pinkHeart : heart} />
-                            </div>
-                            <div className='flex justify-between items-end pt-2'>
-                                <div className='flex my-auto'>
-                                    <img className='' width={'20px'} height={'20px'} src={eye} />
-                                    <span className='ml-2'>8,213</span>
+
+                    <div
+                    className='mt-20 pb-10'
+                        style={{
+                            width: '100%',
+                            display: 'flex',
+                            flexWrap: 'wrap',
+                            gap: '20px',
+                            justifyContent: 'space-between',
+                        }}
+                    >
+                        {data?.data?.length > 0 ? (
+                            data?.data?.map((e, index) => (
+                                <div
+                                    key={e._id}
+                                    style={{
+                                        paddingLeft: '20px',
+                                        paddingRight: '20px',
+                                        outline: 'none',
+                                        userSelect: 'none',
+                                    }}
+                                    className='relative cursor-pointer pb-6 property-container'
+                                >
+                                    <Slider {...settings} className='scroll-container slider-container rounded-8'>
+                                        {e?.images?.map((image) => (
+                                            <img
+                                                key={image}
+                                                onClick={() => navigate(`/propertyDetails/${e._id}`)}
+                                                style={{ width: '40%', height: 'auto', display: 'block' }}
+                                                alt="img"
+                                                src={image}
+                                            />
+                                        ))}
+                                    </Slider>
+
+                                    {e.typeOfRental !== 'superhost' && (
+                                        <div style={{ top: 10, left: 30 }} className='mostLiked'>
+                                            Most liked
+                                        </div>
+                                    )}
+
+                                    <div style={{ position: 'absolute', top: 10, right: 30 }}>
+                                        <img
+                                            style={{ outline: 'none', userSelect: 'none' }}
+                                            alt="wishlist"
+                                            onClick={(el) => {
+                                                handleWishlist(e);
+                                                el.stopPropagation();
+                                                liked.includes(index)
+                                                    ? setLiked(liked.filter((el) => el !== index))
+                                                    : setLiked([...liked, index]);
+                                            }}
+                                            width={'30px'}
+                                            src={liked.includes(index) ? pinkHeart : heart}
+                                        />
+                                    </div>
+
+                                    <div className='flex justify-between items-end pt-2'>
+                                        <div className='flex my-auto'>
+                                            <img className='' width={'20px'} height={'20px'} src={eye} />
+                                            <span className='ml-2'>8,213</span>
+                                        </div>
+                                        <div className='flex my-auto'>
+                                            <img width={'20px'} height={'20px'} src={orangeStar} />
+                                            <span className='ml-2'>3.2</span>
+                                        </div>
+                                    </div>
+
+                                    <p className='location my-auto pt-2'>{e.location}</p>
+                                    <p className='date my-auto pt-1'>Apr 4 - 12</p>
                                 </div>
-                                <div className='flex my-auto'>
-                                    <img width={'20px'} height={'20px'} src={orangeStar} />
-                                    <span className='ml-2'>3.2</span>
-                                </div>
+                            ))
+                        ) : (
+                            <div
+                                className='title'
+                                style={{
+                                    width: '100vw',
+                                    height: '90vh',
+                                    display: 'flex',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                    textAlign: 'center',
+                                }}
+                            >
+                                Loading data...
                             </div>
-                            <p className='location my-auto pt-2'>{e.location}</p>
-                            <p className='date my-auto pt-1'>Apr 4 - 12</p>
-                        </div>
-                    }) : <div className='title'
-                    style={{
-                        width: '100vw',
-                        height: '90vh',
-                        display: 'flex',
-                        justifyContent: 'center',
-                        alignItems: 'center',
-                        textAlign: 'center',
-                    }}
-                >
-                    Loading data...
-                </div>}
+                        )}
                     </div>
                 </div>
             </div>
